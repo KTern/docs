78b6f3d4b5e0032341bba3ab9a15c0974dd0f26b
Gokulnath <sgokulnath@kaartech.com>
2020-09-21 16:58:21 +0530

    Init 1

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
-const CompLibrary = require('../../core/CompLibrary.js');
+const CompLibrary = require("../../core/CompLibrary.js");
 
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
@@ -15,77 +15,103 @@ const GridBlock = CompLibrary.GridBlock;
 
 class HomeSplash extends React.Component {
   render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
+    const { siteConfig, language = "" } = this.props;
+    const { baseUrl, docsUrl } = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+    const langPart = `${language ? `${language}/` : ""}`;
     const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
 
     const SplashContainer = (props) => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
+      <div className='homeContainer'>
+        <div className='homeSplashFade'>
+          <div className='wrapper homeWrapper'>{props.children}</div>
         </div>
       </div>
     );
 
     const Logo = (props) => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
+      <div className='projectLogo'>
+        <img src={props.img_src} alt='Project Logo' />
       </div>
     );
 
     const ProjectTitle = (props) => (
-      <h2 className="projectTitle">
+      <h2 className='projectTitle'>
         {props.title}
         <small>{props.tagline}</small>
       </h2>
     );
 
     const PromoSection = (props) => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
+      <div className='section promoSection'>
+        <div className='promoRow'>
+          <div className='pluginRowBlock'>{props.children}</div>
         </div>
       </div>
     );
 
     const Button = (props) => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
+      <div className='pluginWrapper buttonWrapper'>
+        <a className='button' href={props.href} target={props.target}>
           {props.children}
         </a>
       </div>
     );
 
+    const Block = (props) => (
+      <Container
+        padding={["bottom", "top"]}
+        id={props.id}
+        background={props.background}
+      >
+        <GridBlock
+          align='center'
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
     return (
-      <SplashContainer>
-        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
-        <div className="inner">
-          <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
-          <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection>
-        </div>
-      </SplashContainer>
+      // <SplashContainer>
+      //   <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
+      //   <div className="inner">
+      //     <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
+      //     <PromoSection>
+      //       <Button href="#try">Try It Out</Button>
+      //       <Button href={docUrl('doc1.html')}>Example Link</Button>
+      //       <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+      //     </PromoSection>
+      //   </div>
+      // </SplashContainer>
+      <Block background='light'>
+        {[
+          {
+            content:
+              "Each new Docusaurus project has **randomly-generated** theme colors.",
+            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
+            imageAlign: "right",
+            title: "Randomly Generated Theme Colors",
+          },
+        ]}
+      </Block>
     );
   }
 }
 
 class Index extends React.Component {
   render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
+    const { config: siteConfig, language = "" } = this.props;
+    const { baseUrl } = siteConfig;
 
     const Block = (props) => (
       <Container
-        padding={['bottom', 'top']}
+        padding={["bottom", "top"]}
         id={props.id}
-        background={props.background}>
+        background={props.background}
+      >
         <GridBlock
-          align="center"
+          align='center'
           contents={props.children}
           layout={props.layout}
         />
@@ -94,71 +120,72 @@ class Index extends React.Component {
 
     const FeatureCallout = () => (
       <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
+        className='productShowcaseSection paddingBottom'
+        style={{ textAlign: "center" }}
+      >
         <h2>Feature Callout</h2>
         <MarkdownBlock>These are features of this project</MarkdownBlock>
       </div>
     );
 
     const TryOut = () => (
-      <Block id="try">
+      <Block id='try'>
         {[
           {
             content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
+              "To make your landing page more attractive, use illustrations! Check out " +
+              "[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. " +
+              "The illustrations you see on this page are from unDraw.",
             image: `${baseUrl}img/undraw_code_review.svg`,
-            imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations',
+            imageAlign: "left",
+            title: "Wonderful SVG Illustrations",
           },
         ]}
       </Block>
     );
 
     const Description = () => (
-      <Block background="dark">
+      <Block background='dark'>
         {[
           {
             content:
-              'This is another description of how this project is useful',
+              "This is another description of how this project is useful",
             image: `${baseUrl}img/undraw_note_list.svg`,
-            imageAlign: 'right',
-            title: 'Description',
+            imageAlign: "right",
+            title: "Description",
           },
         ]}
       </Block>
     );
 
     const LearnHow = () => (
-      <Block background="light">
+      <Block background='light'>
         {[
           {
             content:
-              'Each new Docusaurus project has **randomly-generated** theme colors.',
+              "Each new Docusaurus project has **randomly-generated** theme colors.",
             image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
-            imageAlign: 'right',
-            title: 'Randomly Generated Theme Colors',
+            imageAlign: "right",
+            title: "Randomly Generated Theme Colors",
           },
         ]}
       </Block>
     );
 
     const Features = () => (
-      <Block layout="fourColumn">
+      <Block layout='fourColumn'>
         {[
           {
-            content: 'This is the content of my feature',
+            content: "This is the content of my feature",
             image: `${baseUrl}img/undraw_react.svg`,
-            imageAlign: 'top',
-            title: 'Feature One',
+            imageAlign: "top",
+            title: "Feature One",
           },
           {
-            content: 'The content of my second feature',
+            content: "The content of my second feature",
             image: `${baseUrl}img/undraw_operating_system.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two',
+            imageAlign: "top",
+            title: "Feature Two",
           },
         ]}
       </Block>
@@ -178,15 +205,15 @@ class Index extends React.Component {
         ));
 
       const pageUrl = (page) =>
-        baseUrl + (language ? `${language}/` : '') + page;
+        baseUrl + (language ? `${language}/` : "") + page;
 
       return (
-        <div className="productShowcaseSection paddingBottom">
+        <div className='productShowcaseSection paddingBottom'>
           <h2>Who is Using This?</h2>
           <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
+          <div className='logos'>{showcase}</div>
+          <div className='more-users'>
+            <a className='button' href={pageUrl("users.html")}>
               More {siteConfig.title} Users
             </a>
           </div>
@@ -197,14 +224,14 @@ class Index extends React.Component {
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
-        <div className="mainContainer">
+        {/* <div className='mainContainer'>
           <Features />
           <FeatureCallout />
           <LearnHow />
           <TryOut />
           <Description />
           <Showcase />
-        </div>
+        </div> */}
       </div>
     );
   }
