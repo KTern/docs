99763fe558dc5200310b187103ca966812cca545
Edson Frainlar <fedson@kaartech.com>
2020-12-01 15:05:11 +0530

    updated: siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,52 +10,52 @@
 
 // List of projects/orgs using your project for the users page.
 const users = [
-  {
-    caption: "User1",
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: "/img/undraw_open_source.svg",
-    infoLink: "https://www.facebook.com",
-    pinned: true,
-  },
+	{
+		caption: 'User1',
+		// You will need to prepend the image path with your baseUrl
+		// if it is not '/', like: '/test-site/img/image.jpg'.
+		image: '/img/undraw_open_source.svg',
+		infoLink: 'https://www.facebook.com',
+		pinned: true,
+	},
 ];
 
 const siteConfig = {
-  title: "", // Title for your website.
-  tagline: "SAP S/4HANA Accelerator",
-  // url: "https://docs.ktern.com", // Your website URL
-  // baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  url: "https://ktern-ai.github.io/",
-  baseUrl: "/",
+	title: '', // Title for your website.
+	tagline: 'KTern.AI - Help Documentation',
+	// url: "https://docs.ktern.com", // Your website URL
+	// baseUrl: "/", // Base URL for your project */
+	// For github.io type URLs, you would set the url and baseUrl like:
+	url: 'https://ktern-ai.github.io/',
+	baseUrl: '/',
 
-  // Used for publishing and more
-  projectName: "product-documentation",
-  organizationName: "ktern-ai",
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+	// Used for publishing and more
+	projectName: 'product-documentation',
+	organizationName: 'ktern-ai',
+	// For top-level user or org sites, the organization is still the same.
+	// e.g., for the https://JoelMarcey.github.io site, it would be set like...
+	//   organizationName: 'JoelMarcey'
 
-  docsSideNavCollapsible: true,
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [],
+	docsSideNavCollapsible: true,
+	// For no header links in the top nav bar -> headerLinks: [],
+	headerLinks: [],
 
-  // If you have users set above, you add it here:
-  users,
+	// If you have users set above, you add it here:
+	users,
 
-  /* path to images for header/footer */
-  headerIcon: "img/favicon.ico",
-  footerIcon: "img/favicon.ico",
-  favicon: "img/titlebar.ico",
+	/* path to images for header/footer */
+	headerIcon: 'img/favicon.ico',
+	footerIcon: 'img/favicon.ico',
+	favicon: 'img/titlebar.ico',
 
-  /* Colors for website */
-  colors: {
-    primaryColor: "#808080",
-    secondaryColor: "#16525d",
-  },
+	/* Colors for website */
+	colors: {
+		primaryColor: '#d3d3d3',
+		secondaryColor: '#16525d',
+	},
 
-  /* Custom fonts for website */
-  /*
+	/* Custom fonts for website */
+	/*
   fonts: {
     myFont: [
       "Times New Roman",
@@ -68,41 +68,41 @@ const siteConfig = {
   },
   */
 
-  gaTrackingId: "UA-130499835-1",
+	gaTrackingId: 'UA-130499835-1',
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+	// This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+	copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "default",
-  },
+	highlight: {
+		// Highlight.js theme to use for syntax highlighting in code blocks.
+		theme: 'default',
+	},
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ["https://buttons.github.io/buttons.js"],
+	// Add custom scripts here that would be placed in <script> tags.
+	scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
-  onPageNav: "separate",
-  // No .html extensions for paths.
-  cleanUrl: true,
+	// On page navigation for the current documentation page.
+	onPageNav: 'separate',
+	// No .html extensions for paths.
+	cleanUrl: true,
 
-  // Open Graph and Twitter card images.
-  ogImage: "img/undraw_online.svg",
-  twitterImage: "img/undraw_tweetstorm.svg",
+	// Open Graph and Twitter card images.
+	ogImage: 'img/undraw_online.svg',
+	twitterImage: 'img/undraw_tweetstorm.svg',
 
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+	// For sites with a sizable amount of content, set collapsible to true.
+	// Expand/collapse the links and subcategories under categories.
+	// docsSideNavCollapsible: true,
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+	// Show documentation's last contributor's name.
+	// enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+	// Show documentation's last update time.
+	// enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  // repoUrl: 'https://github.com/facebook/test-site',
+	// You may provide arbitrary config keys to be used as needed by your
+	// template. For example, if you need your repo's URL...
+	// repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
